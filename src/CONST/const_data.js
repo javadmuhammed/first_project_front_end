046523997cb9328e857646af199c21d7fb494d14
javadmuhammed <155625545+javadmuhammed@users.noreply.github.com>
2024-01-08 20:09:42 +0530

    Update const_data.js

--- src/CONST/const_data.js
+++ src/CONST/const_data.js
@@ -2,11 +2,11 @@
 export const const_data = {
     CURRENCY_ICON: "₹",
     CURRENCY_TYPE: "INR",
-    API_URL: "api.veguess.shop",
-    FRONT_END_DOMAIN: "veguess.shop",
+    API_URL: "www.api.veguess.shop",
+    FRONT_END_DOMAIN: "www.veguess.shop",
     ADMIN_PATH: "/admin",
-    user_profile_path: "api.veguess.shop/images/userProfile",
-    public_image_url: "api.veguess.shop/images/web_images",
+    user_profile_path: "www.api.veguess.shop/images/userProfile",
+    public_image_url: "www.api.veguess.shop/images/web_images",
     API_ENDPOINT: {
         signup_user: "/sign_up",
         verify_user_otp: "/otp_validation",
